e7794c41ed613d67c40a24630310983644674192
mrowling <mitchell.rowling@gmail.com>
2026-03-13 11:27:08 +1100

    Support local development mode for carelink plugin
    - Add TMUX_LOCAL_DEV environment variable support
    - When set, uses ~/code/tmux-carelink-go instead of installed plugin
    - Facilitates development and testing of carelink plugin changes

--- scripts/carelink.sh
+++ scripts/carelink.sh
@@ -5,7 +5,13 @@
 
 main() {
   # Call the actual carelink plugin script
-  local carelink_plugin_script="$HOME/.config/tmux/plugins/tmux-carelink-go/scripts/carelink.sh"
+  # Use local development version if TMUX_LOCAL_DEV is set
+  local carelink_plugin_script
+  if [[ -n "$TMUX_LOCAL_DEV" ]] && [[ -d "$HOME/code/tmux-carelink-go" ]]; then
+    carelink_plugin_script="$HOME/code/tmux-carelink-go/scripts/carelink.sh"
+  else
+    carelink_plugin_script="$HOME/.config/tmux/plugins/tmux-carelink-go/scripts/carelink.sh"
+  fi
   
   if [[ -x "$carelink_plugin_script" ]]; then
     "$carelink_plugin_script"
